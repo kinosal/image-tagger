f9ae65d7f95e598dae5c2a8f75d7580ae9491f20
Nikolas Schriefer <info@nikolasschriefer.de>
2023-11-25 14:56:29 +0100

    Handle app states

--- app.py
+++ app.py
@@ -15,19 +15,24 @@ logging.basicConfig(format="\n%(asctime)s\n%(message)s", level=logging.INFO, for
 # Define functions
 def detect_objects(image_file, components):
     """Detect objects in images."""
+    st.session_state.error = ""
+    st.session_state.image_file = None
+    st.session_state.labels = []
     with spinner_placeholder:
-        hashed_image, hashed_image_name = aws.hash_and_scale_image(
-            mode="file", image_file=image_file
-        )
-        if not aws.find_image(hashed_image_name):
-            image_url = aws.upload_image(
-                mode="file", image_name=hashed_image_name, image_file=hashed_image
+        with st.spinner("Analyzing image..."):
+            hashed_image, hashed_image_name = aws.hash_and_scale_image(
+                mode="file", image_file=image_file
             )
-        else:
-            image_url = f"https://{aws.BUCKET}.s3.amazonaws.com/{hashed_image_name}"
-        labels = gpt.detect_labels(image_url, components)
-        logging.info(labels)
-        st.session_state.labels = labels
+            if not aws.find_image(hashed_image_name):
+                image_url = aws.upload_image(
+                    mode="file", image_name=hashed_image_name, image_file=hashed_image
+                )
+            else:
+                image_url = f"https://{aws.BUCKET}.s3.amazonaws.com/{hashed_image_name}"
+            labels = gpt.detect_labels(image_url, components)
+            logging.info(labels)
+            st.session_state.image_file = image_file
+            st.session_state.labels = labels
 
 
 # Configure Streamlit page and state
@@ -36,6 +41,8 @@ if "labels" not in st.session_state:
     st.session_state.labels = []
 if "error" not in st.session_state:
     st.session_state.error = ""
+if "image_file" not in st.session_state:
+    st.session_state.image_file = None
 
 # Render Streamlit page
 st.title("Tag image")
@@ -64,7 +71,7 @@ spinner_placeholder = st.empty()
 if st.session_state.error:
     st.error(st.session_state.error)
 
-if st.session_state.labels:
+if st.session_state.labels and st.session_state.image_file == image_file:
     st.markdown("""---""")
     col1, col2 = st.columns([1, 2])
     with col1:
